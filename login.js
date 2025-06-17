bd6caf61a6b2e93634f3a086441c5b6dc6091e8e
AxxxWorks <axxxworks@gmail.com>
2025-06-17 12:23:04 -0600

    Update login.js

--- login.js
+++ login.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // --- PUNTO CLAVE 1 ---
     // Pega aquí la configuración de Firebase que copiaste de la consola.
     // ¡ASEGÚRATE DE RELLENAR ESTO CON TUS CLAVES REALES!
-    const firebaseConfig = {
-        apiKey: "TU_API_KEY",
-        authDomain: "TU_AUTH_DOMAIN",
-        projectId: "TU_PROJECT_ID",
-        storageBucket: "TU_STORAGE_BUCKET",
-        messagingSenderId: "TU_MESSAGING_SENDER_ID",
-        appId: "TU_APP_ID"
-    };
+   var firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "PROJECT_ID",
+  // The value of `storageBucket` depends on when you provisioned your default bucket (learn more)
+  storageBucket: "PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  measurementId: "G-MEASUREMENT_ID",
+};
 
     // Inicializar Firebase
     firebase.initializeApp(firebaseConfig);
